7d09ae3ac9de58c5b346ccd235c2e766cdd1374c
Matt Gordon <matt@expectedbehavior.com>
2010-09-19 15:20:36 -0400

    add handy stuff to cucumber env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,3 +7,27 @@ ActionController::Base.allow_rescue = false
 
 require 'cucumber/formatter/unicode'
 require 'cucumber/rails/rspec'
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
+DatabaseCleaner.strategy = :truncation
+
+require 'cucumber/rails/world'
+require 'cucumber/rails/active_record'
+require 'cucumber/web/tableish'
+
+require 'fileutils'
+FileUtils.touch "#{RAILS_ROOT}/tmp/restart.txt"
+
+@@cucumber_cli_test_number = 1
+Before do
+  puts "--- TEST ##{@@cucumber_cli_test_number} ---"
+  @@cucumber_cli_test_number += 1
+end
+
+require 'factory_girl'
+require "#{RAILS_ROOT}/test/factories.rb"
+require "#{RAILS_ROOT}/db/bootstrap.rb"
+
+Before do
+  Bootstrapper.run :test
+end
