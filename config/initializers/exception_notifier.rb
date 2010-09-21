df336924a8c708f57c9d71788fbe3edad44d8878
Matt Gordon <matt@expectedbehavior.com>
2010-09-20 21:36:25 -0400

    removing exception notifier; it is lame

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,1 +1,1 @@
-ExceptionNotifier.exception_recipients = %w(joel@expectedbehavior.com michael@expectedbehavior.com matt@expectedbehavior.com jason@expectedbehavior.com)
+# ExceptionNotifier.exception_recipients = %w(joel@expectedbehavior.com michael@expectedbehavior.com matt@expectedbehavior.com jason@expectedbehavior.com)
