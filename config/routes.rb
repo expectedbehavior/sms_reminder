3f964c27918fdfd83fd354a578adcea8d05cc65f
Matt Gordon <matt@expectedbehavior.com>
2010-09-20 21:17:02 -0400

    adding the reminders controller so I can see what kind of crap they're sending me

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :reminders
+
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
   map.signup "signup", :controller => "users", :action => "new"
